0181df4d90ebc67af6da6d75b55ceb1bfde02348
marcofilho <marco.filho@ess.eu>
2025-05-01 17:04:57 +0200

    Use portfile in run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+ORANGE='\033[0;33m'
+END_COLOR='\033[0m'
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <ip:port>"
-    exit 1
+    # echo "Usage: $0 <Device_portfile>"
+    # exit 1
+    echo -e "${ORANGE}Warning!${END_COLOR} No argument."
+    echo -e "Assuming portfile = DeviceSim0.port"
+    echo -e "If you want to change that: "
+    echo -e "Usage: $0 <Device_portfile>\n\n\n"
+    portfile="DeviceSim0.port"
+else
+    portfile=$1
 fi
 
-ADDRESS="$1"
+ADDRESS=$(<$portfile)
 thispath="$(dirname -- "${BASH_SOURCE[0]}")"
 thispath="$(cd -- "$thispath" && pwd)"
 
